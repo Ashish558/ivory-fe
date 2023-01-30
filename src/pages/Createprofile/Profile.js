cc1dc7d59ac035df0e661a95d6d4caaf1470983a
nipunkhattri321 <nipunkhattri321@gmail.com>
2023-01-30 20:59:41 +0530

    added module.css and usestate used

--- src/pages/Createprofile/Profile.js
+++ src/pages/Createprofile/Profile.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import arrow from "../../assets/arrow_back.png"
-import "./Profile.css"
+import styles from "./Profile.module.css"
 import photo from "../../assets/smile.png"
 import cross from "../../assets/cross.png"
 import img from "../../assets/iphoto.png"
@@ -8,149 +8,168 @@ import { Link } from 'react-router-dom'
 const Profile = () => {
 
   const [email,setemail] = useState("");
+  const [addtext,settext] = useState("");
+  const [showdiv,setshowdiv]=useState(false);
+  const [addnewtextdiv,setaddnewtextdiv]=useState(false);
+  const [textColor,setTextColor]=useState('white');
+  const [backcolor,setbackcolor]=useState('#FFFFFF');
+  console.log(addtext);
+  // let arr = addtext.split(' '); 
+  // console.log(arr);
   const [gender,setgender] = useState("");
-  const [interest,setinterest] = useState([]);
+  const colorchange =()=>{
+    setTextColor('Blue')
+  }
+  // const Backcolor = ()=>{
+  //   setbackcolor('blue')
+  // }
+  const [interest,setinterest] = useState([
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    },
+    {
+        name:"dknkmvcam",
+        selected:false
+    }
+  ])
+  const [selec,setselec] = useState("true");
   console.log(email);
   console.log(gender);
-
   //------call to next page hide and show div----------
-  const openinterest = ()=>{
-      const divv = document.querySelector(".all-interest");
-      const divvback = document.querySelector(".data-input");
-      divv.style.display = "block";
-      divvback.style.backgroundColor = "rgb(154 175 196)";
+    const openinterest = ()=>{
+        setshowdiv(true);
+        setbackcolor('rgb(145 165 186)')
     }
     const crossbox = ()=>{
-      const divv = document.querySelector(".all-interest");
-      // divv.style.display = "none";
-      divv.style.display = "none";
-      const divvback = document.querySelector(".data-input");
-      divvback.style.backgroundColor = "white";
+        setshowdiv(false)
+        setbackcolor('#FFFFFF')
     }
-    const addnew = () =>{
-      const newdivv = document.querySelector(".add-text");
-      newdivv.style.display = "block";
+    const addnew= () =>{
+        setaddnewtextdiv(true)
+        setbackcolor('rgb(145 165 186)')
     }
     const addcrossbox = ()=>{
-      const newdivv = document.querySelector(".add-text");
-      // divv.style.display = "none";
-      newdivv.style.display = "none";
+        setaddnewtextdiv(false);
+        // setbackcolor('#001C38')
     }
-
   // ------------------------------------------------------------
   return (
     <div>
     {/* ----------------------main detail form ----------------------*/}
-    <div className='data-input'>
-    <div className="head">
+    <div className={styles.datainput} style={{background:backcolor}}>
+    <div className={styles.head}>
       <img src={arrow} alt="" />
-     <p className='edit-head'>Edit Your Profile</p> 
+     <p className={styles.edithead}>Edit Your Profile</p> 
     </div> 
-    <div className='image'>
-        <div className='ssmmillee'>
-          <img src={photo} className="img2" alt="" />
+    <div className={styles.image}>
+        <div className={styles.ssmmillee}>
+          <img src={photo} className={styles.img2} alt="" />
         </div>
     </div>
-    <div className="form">
-      <div className='input1'>
-        <label htmlFor=""className='email-label' >Email Address</label>
-        <input type="text" placeholder='xyz@gmail.com' className='email-input' name="email" onChange={(e)=>setemail(e.target.value)}/>
+    <div className={styles.form}>
+      <div className={styles.input1}>
+        <label htmlFor=""className={styles.emaillabel} >Email Address</label>
+        <input type="text" placeholder='xyz@gmail.com' className={styles.emailinput} name="email" onChange={(e)=>setemail(e.target.value)}/>
       </div>
-      <div className='input2'>
-        <label htmlFor=""className='email-label' >Gender</label>
-        <div className='gender-input'>
-          <div className='checkbox-div-1'>
-          <label className='gender-male' htmlFor="">Male</label>
-        <input  type="radio" name='gender' placeholder='Male'  onChange={(e)=>setgender(e.target.value)} className='male-input'/>
+      <div className={styles.input2}>
+        <label htmlFor=""className={styles.emaillabel} >Gender</label>
+        <div className={styles.genderinput}>
+          <div className={styles.checkboxdiv1}>
+          <label className={styles.gendermale} htmlFor="">Male</label>
+        <input label="Male" checked={gender === 'Male'} value="Male" type="radio" onChange={(e)=>setgender(e.target.value)} className={styles.maleinput}/>
           </div>
-          <div className='checkbox-div-2'>
-          <label className='gender-Female' htmlFor="">Female</label>
-        <input type="radio" placeholder='Female' name='gender' onChange={(e)=>setgender(e.target.value)} className='female-input'/>
+          <div className={styles.checkboxdiv2}>
+          <label className={styles.genderFemale} htmlFor="">Female</label>
+        <input label="Female" checked={gender === 'Female'} value="Female" type="radio" onChange={(e)=>setgender(e.target.value)} className={styles.femaleinput}/>
         </div>
           </div>
       </div>
-      <div className='input1'>
-        <label htmlFor=""className='email-label'  >Interests</label>
-        <input type="text" placeholder='Click to choose' onClick={openinterest}  className='email-input'/>{/*-------------Open interest page selecting---------------*/}
+      <div className={styles.input1}>
+        <label htmlFor="" className={styles.emaillabel} >Interests</label>
+        <div type="text" placeholder='Click to choose' onClick={openinterest}  className={styles.chooseinput}>
+        {/* {
+            arr?.map((ele)=>{
+                return(
+                   <div className={styles.divaddinput}>
+                    <p>{ele}</p>
+                    </div>
+                )
+            })
+        } */}
+            </div>{/*-------------Open interest page selecting---------------*/}
       </div>
     </div>
-    <button className='btn-Update'>Update</button>{/*-------------Go to next page---------------*/}
+    <button className={styles.btnUpdate}>Update</button>{/*-------------Go to next page---------------*/}
     </div>
     {/* ------------------------------------------------------------------------------ */}
     {/* ---------------------------The interest selecting div------------------------------------ */}
-    <div className='all-interest'>
-    <div className="int">
-      <img src={cross} onClick={crossbox} alt="" className='close-interest'/>{/*-------------Cross the open interest page selecting---------------*/}
-      <p className='intp'>Interest</p>
+    {showdiv==true?
+    <div className={styles.allinterest}>
+    <div className={styles.int}>
+      <img src={cross} onClick={crossbox} alt="" className={styles.closeinterest}/>{/*-------------Cross the open interest page selecting---------------*/}
+      <p className={styles.intp}>Interest</p>
       </div>
-    <hr className='brk'/>
-    <p className='cho'>Choose one or more:</p>
-    <div className='int-topic'> 
-    <div className='inttopic'>
-        <div className="butt1">
-          <img src={img} alt="" />
-          <h3>Socializing</h3>
-        </div>
-        <div className="butt2">
-          <img src={img} alt="" />
-          <h3>Community work</h3>
-        </div>
-</div>
-<div className='inttopic'>
-<div className="butt3">
-  <img src={img} alt="" />
-  <h3>Photography</h3>
-</div>
-<div className="butt4">
-  <img src={img} alt="" />
-  <h3>Writing</h3>
-</div>
+    <hr className={styles.brk}/>
+    <p className={styles.cho}>Choose one or more:</p>
+    <div className={styles.intttopic}> 
+        {
+            interest?.map((ele,index)=>{
+                return(
+                // <div className={styles.inttopic}>
+                    <span className={styles.butt1}  style={{background:textColor}} onClick={colorchange}>
+                    <img src={img} alt="" />
+                    <h3>{ele.name}</h3>
+                    </span>
+                )
+            })
+        }
     </div>
-    <div className='inttopic'>
-
-
-<div className="butt5">
-  <img src={img} alt="" />
-  <h3>Gardening</h3>
-</div>
-<div className="butt6">
-  <img src={img} alt="" />
-  <h3>Arts & Crafts</h3>
-</div>
-</div>
-<div className='inttopic'>
-
-            <div className="butt5">
-              <img src={img} alt="" />
-              <h3>Mythology</h3>
-            </div>
-            <div className="butt6">
-              <img src={img} alt="" />
-              <h3>Technology</h3>
-            </div>
-        </div>
-        <button className='addint' onClick={addnew}>Add</button>{/*-------------Add Your interest page open---------------*/}
-        <hr className='hend'/>
-        <Link to="/"><p  className='more'>Suggest more interest categories.</p></Link>
-    </div>
-    </div>
+        <button className={styles.addint} onClick={addnew}>Add</button>{/*-------------Add Your interest page open---------------*/}
+        <hr className={styles.hend}/>
+        <Link to="/"><p  className={styles.more}>Suggest more interest categories.</p></Link>
+    </div>:<></>
+    }
     {/* ------------------------------------------------------------- */}
     {/* -----------------------------------Add your interest div------------------------------------------------ */}
-    <div className='add-text'>
-        <div className="int">
-      <img src={cross} onClick={addcrossbox} alt="" className='close-interest'/>{/*-------------Close Add Your interest page ---------------*/}
-      <p className='intp'>Interest</p>
+    {
+        addnewtextdiv ==true?
+    <div className={styles.addtext}>
+        <div className={styles.int}>
+      <img src={cross} onClick={addcrossbox} alt="" className={styles.closeinterest}/>{/*-------------Close Add Your interest page ---------------*/}
+      <p className={styles.intp}>Interest</p>
         </div>
-        <hr className='head1'/>
-        <div className="para">
+        <hr className={styles.head1}/>
+        <div className={styles.para}>
       <p>Didn't find your top interests?No worries! Let us know and we will 
       try our best to add more relavent
       categories:
       </p>
-      <input type="text"  className='parainput' placeholder='Type here..'/>
+      <input type="text" name='addtext' onChange={(e)=>settext(e.target.value)} className={styles.parainput} placeholder='Type here..'/>
       </div>
-      <button className='send'>Send</button>
-    </div>
+      <button className={styles.send}>Send</button>
+    </div>:<></>
+    }
     {/* ------------------------------------------------------------------------------------------------------- */}
     </div>
   )
