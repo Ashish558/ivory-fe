bff99c3ca88f336e987f240628fc4564a80d9eb7
Ashish558 <ashishprasad2001ap@gmail.com>
2023-02-04 20:40:21 +0530

    activity fix

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -4,7 +4,7 @@ import styles from './slider.module.css'
 import SecondaryButton from '../../components/Buttons/SecondaryButton'
 import CatImg from '../../assets/images/cat.png'
 
-export default function Feedback({ created_at, activity, submission, feedbacks, is_feedbacked, currentIndex, idx }) {
+export default function Feedback({ created_at, activity, submission, feedbacks, is_feedbacked, currentIndex, idx, onView }) {
    return (
       <div className={`${styles.container}  ${currentIndex === idx ? styles.active : idx < currentIndex ? styles.prev : styles.next}`}>
          <p className='text-lightGray text-sm font-normal mb-3.5 mt-4 px-4 pt-4'>
@@ -15,7 +15,7 @@ export default function Feedback({ created_at, activity, submission, feedbacks, 
                <img src={submission} alt='submission' />
             </div>
             <div className='flex flex-col ml-4'>
-               <SecondaryButton children='View'
+               <SecondaryButton children='View' onClick={()=>onView(submission)}
                   className='text-sm shadow-light w-[68px] h-8 px-2 pt-1 pb-1 mb-2 border-none' />
                <SecondaryButton children='Share'
                   className='text-sm w-[68px] h-8 shadow-light px-2 pt-1 pb-1 mb-2 border-none' />
