b8a883030152e0211524201dbd03890630490821
Ashish558 <ashishprasad2001ap@gmail.com>
2023-02-04 13:35:16 +0530

    flow changed

--- src/pages/splash/LogoLanding.js
+++ src/pages/splash/LogoLanding.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import styles from "./LogoLanding.module.css"; 
+import React, { useEffect } from 'react'
+import styles from "./LogoLanding.module.css";
 import logo from "../../assets/logo.png";
 import { useNavigate } from 'react-router-dom';
 import slideimage1 from "../../assets/slideimage1.png"
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 const LogoLanding = () => {
   const navigate = useNavigate();
-  setTimeout(() => {
-    navigate("/landing")
-  }, 2000);
+  const { width } = useWindowDimensions()
+
+  useEffect(() => {
+    if (width > 990) {
+      navigate("/home")
+    } else {
+      setTimeout(() => {
+        navigate("/landing")
+      }, 2000)
+    }
+  }, [])
+
   return (
     <>
-    <img src={slideimage1} className={styles.imgg} alt="" />
-    <div className={styles.logoland}>
+      <img src={slideimage1} className={styles.imgg} alt="" />
+      <div className={styles.logoland}>
         <img src={logo} alt="" />
-    </div> 
+      </div>
     </>
   )
 }
