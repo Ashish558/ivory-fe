4e95903653827b504d26da38a34c2330b33d1965
Ashish558 <ashishprasad2001ap@gmail.com>
2023-02-04 18:16:01 +0530

    modal added in create profile

--- src/pages/Createprofile/Profile.js
+++ src/pages/Createprofile/Profile.js
@@ -10,11 +10,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addInterest, getInterests } from '../../services/activities'
 import { editProfile, uploadProfile } from '../../services/user'
 import { updateProfileData } from '../../redux/slices/user'
+import Modal from '../../components/Modal/modal'
 
 const Profile = () => {
   const [name, setName] = useState('')
   const [mobile_no, setMobile_no] = useState('')
-  const [email,setemail] = useState("");
+  const [email, setemail] = useState("");
   const [text, settext] = useState("");
   const [showdiv, setshowdiv] = useState(false);
   const [addnewtextdiv, setaddnewtextdiv] = useState(false);
@@ -178,158 +179,171 @@ const Profile = () => {
   console.log('text', interestInput);
   // ------------------------------------------------------------
   return (
-    <div className=''>
-      {/* ----------------------main detail form ----------------------*/}
-      <div className={styles.datainput} style={{ background: backcolor }}>
-        <div className={styles.navbar}>
+    <>
+      <div className=''>
+        {/* ----------------------main detail form ----------------------*/}
+        <div className={styles.datainput} style={{ background: backcolor }}>
+          <div className={styles.navbar}>
 
-        </div>
-        <div className={styles.head}>
-          <img src={arrow} alt="" onClick={() => navigate(-1)} />
-          <p className={styles.edithead}>Edit Your Profile</p>
-        </div>
-        <div className={styles.image}>
-          <div className={styles.ssmmillee}>
-            <img src={profileData.profile_picture ? profileData.profile_picture : photo}
-              className={styles.img2} alt=""
-              onClick={() => photoRef.current.click()} />
-            <input className='hidden' type='file' accept="image/png, image/gif, image/jpeg" ref={photoRef}
-              onChange={(e) => handlePhotoUpload(e)} />
           </div>
-          <p className={`cursor-pointer ${styles.addpit}`}
-            onClick={() => photoRef.current.click()}
+          <div className={styles.head}>
+            <img src={arrow} alt="" onClick={() => navigate(-1)} />
+            <p className={styles.edithead}>Edit Your Profile</p>
+          </div>
+          <div className={styles.image}>
+            <div className={styles.ssmmillee}>
+              <img src={profileData.profile_picture ? profileData.profile_picture : photo}
+                className={styles.img2} alt=""
+                onClick={() => photoRef.current.click()} />
+              <input className='hidden' type='file' accept="image/png, image/gif, image/jpeg" ref={photoRef}
+                onChange={(e) => handlePhotoUpload(e)} />
+            </div>
+            <p className={`cursor-pointer ${styles.addpit}`}
+              onClick={() => photoRef.current.click()}
 
-          >Add Profile picture
-          </p>
-        </div>
-        <div className={styles.formimg}>
-          <div className={styles.form}>
-            <div className={styles.input1}>
-              <label htmlFor="" className={styles.emaillabel} >Name</label>
-              <input type="text" placeholder='Sahil Wadhwa' value={name}
-                name="name"
-                className={styles.emailinput}
-                onChange={(e) => setName(e.target.value)} />
-            </div>
-            <div className={styles.input1}>
-              <label htmlFor="" className={styles.emaillabel} >Phone Number</label>
-              <input type="Number" placeholder='9777766665' className={styles.emailinput} 
-              value={mobile_no} name="mobile_no" onChange={(e) => setMobile_no(e.target.value)} />
-            </div>
-            <div className={styles.input1}>
-              <label htmlFor="" className={styles.emaillabel} >Email Address</label>
-              <input type="email" placeholder='xyz@gmail.com' className={styles.emailinput} name="email"
-                value={email} onChange={(e) => setemail(e.target.value)} />
-            </div>
-            <div className={styles.input2}>
-              <label htmlFor="" className={styles.emaillabel} >Gender</label>
-              <div className={styles.genderinput}>
-                <div className={styles.checkboxdiv1}>
-                  <label className={styles.gendermale} htmlFor="">Male</label>
-                  <input label="Male" value="male" type="checkbox"
-                    checked={gender === 'male' ? true : false}
-                    onChange={(e) => setgender(e.target.value)} className={styles.maleinput}
-                  />
-                </div>
-                <div className={styles.checkboxdiv2}>
-                  <label className={styles.genderFemale} htmlFor="">Female</label>
-                  <input label="Female" checked={gender === 'female'} value='female' type="checkbox" onChange={(e) => setgender(e.target.value)} className={styles.femaleinput} />
+            >Add Profile picture
+            </p>
+          </div>
+          <div className={styles.formimg}>
+            <div className={styles.form}>
+              <div className={styles.input1}>
+                <label htmlFor="" className={styles.emaillabel} >Name</label>
+                <input type="text" placeholder='Sahil Wadhwa' value={name}
+                  name="name"
+                  className={styles.emailinput}
+                  onChange={(e) => setName(e.target.value)} />
+              </div>
+              <div className={styles.input1}>
+                <label htmlFor="" className={styles.emaillabel} >Phone Number</label>
+                <input type="Number" placeholder='9777766665' className={styles.emailinput}
+                  value={mobile_no} name="mobile_no" onChange={(e) => setMobile_no(e.target.value)} />
+              </div>
+              <div className={styles.input1}>
+                <label htmlFor="" className={styles.emaillabel} >Email Address</label>
+                <input type="email" placeholder='xyz@gmail.com' className={styles.emailinput} name="email"
+                  value={email} onChange={(e) => setemail(e.target.value)} />
+              </div>
+              <div className={styles.input2}>
+                <label htmlFor="" className={styles.emaillabel} >Gender</label>
+                <div className={styles.genderinput}>
+                  <div className={styles.checkboxdiv1}>
+                    <label className={styles.gendermale} htmlFor="">Male</label>
+                    <input label="Male" value="male" type="checkbox"
+                      checked={gender === 'male' ? true : false}
+                      onChange={(e) => setgender(e.target.value)} className={styles.maleinput}
+                    />
+                  </div>
+                  <div className={styles.checkboxdiv2}>
+                    <label className={styles.genderFemale} htmlFor="">Female</label>
+                    <input label="Female" checked={gender === 'female'} value='female' type="checkbox" onChange={(e) => setgender(e.target.value)} className={styles.femaleinput} />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className={styles.input1}>
-              <label htmlFor="" className={styles.emaillabel} onClick={openinterest} >Interests</label>
+              <div className={styles.input1}>
+                <label htmlFor="" className={styles.emaillabel} onClick={openinterest} >Interests</label>
 
-              {
-                userInterests.length === 0 ?
-                  <div type="text" placeholder='Click to choose' onClick={openinterest}
-                    className={styles.chooseinput}>
-                  </div>
-                  :
-                  <div className='h-[85px] py-3 flex flex-wrap items-center gap-x-3 gap-y-3 border border-[#939CA3] overflow-auto px-4'>
-                    {
-                      userInterests.map(int => {
-                        return <div className='bg-[#BDF4FF] py-1.5 px-3  rounded-[8px]'>
-                          {int.name}
-                        </div>
-                      })
-                    }
-                  </div>
-              }
-              {/* <div type="text" placeholder='Click to choose' onClick={openinterest}
+                {
+                  userInterests.length === 0 ?
+                    <div type="text" placeholder='Click to choose' onClick={openinterest}
+                      className={styles.chooseinput}>
+                    </div>
+                    :
+                    <div className='h-[85px] py-3 flex flex-wrap items-center gap-x-3 gap-y-3 border border-[#939CA3] overflow-auto px-4'>
+                      {
+                        userInterests.map(int => {
+                          return <div className='bg-[#BDF4FF] py-1.5 px-3  rounded-[8px]'>
+                            {int.name}
+                          </div>
+                        })
+                      }
+                    </div>
+                }
+                {/* <div type="text" placeholder='Click to choose' onClick={openinterest}
                 className={styles.chooseinput}>
 
               </div> */}
-              {/*-------------Open interest page selecting---------------*/}
+                {/*-------------Open interest page selecting---------------*/}
+              </div>
             </div>
+            <img src={ivoryforming} className={styles.ivoryForm} alt="" />
           </div>
-          <img src={ivoryforming} className={styles.ivoryForm} alt="" />
+          <button className={styles.btnUpdate} onClick={handleSubmit}>Save Profile</button>{/*-------------Go to next page---------------*/}
         </div>
-        <button className={styles.btnUpdate} onClick={handleSubmit}>Save Profile</button>{/*-------------Go to next page---------------*/}
       </div>
       {/* ------------------------------------------------------------------------------ */}
       {/* ---------------------------The interest selecting div------------------------------------ */}
       {showdiv == true ?
-        <div className={styles.allinterest}>
-          <div className={styles.int}>
-            <img src={cross} onClick={crossbox} alt="" className={styles.closeinterest} />{/*-------------Cross the open interest page selecting---------------*/}
-            <p className={styles.intp}>Interests</p>
-          </div>
-          <hr className={styles.brk} />
-          <p className={styles.cho}>Choose one or more:</p>
-          <div className={styles.intttopic}>
-            {
-              interest?.map((ele, index) => {
-                return (
-                  // <div className={styles.inttopic}>
-                  <span className={styles.butt1} style={{ background: textColor }} onClick={colorchange}>
-                    <img src={img} alt="" />
-                    <h3>{ele.name}</h3>
-                  </span>
-                )
-              })
-            }
-            {
-              allInterests.map(int => {
-                return <div className={`${styles.butt1} ${int.selected === true ? `${styles.interestSelected}` : ''} `}
-                  key={int.id} onClick={() => toggleInt(int.id)} >
-                  <img src={int.icon} alt="" />
-                  <h3> {int.name} </h3>
-                </div>
-              })
-            }
-          </div>
-          <button className={styles.addint} onClick={addnew}>Add</button>{/*-------------Add Your interest page open---------------*/}
-          <hr className={styles.hend} />
-          <Link to="/"><p className={styles.more}>Suggest more interest categories.</p></Link>
-        </div> : <></>
+
+        <Modal classname='max-w-[434px] rounded-[20px]' body={
+          <>
+            <div className={styles.int}>
+              <img src={cross} onClick={crossbox} alt="" className={styles.closeinterest} />{/*-------------Cross the open interest page selecting---------------*/}
+              <p className={styles.intp}>Interests</p>
+            </div>
+            <hr className={styles.brk} />
+            <p className={styles.cho}>Choose one or more:</p>
+            <div className={styles.intttopic}>
+              {
+                interest?.map((ele, index) => {
+                  return (
+                    // <div className={styles.inttopic}>
+                    <span className={styles.butt1} style={{ background: textColor }} onClick={colorchange}>
+                      <img src={img} alt="" />
+                      <h3>{ele.name}</h3>
+                    </span>
+                  )
+                })
+              }
+              {
+                allInterests.map(int => {
+                  return <div className={`${styles.butt1} ${int.selected === true ? `${styles.interestSelected}` : ''} `}
+                    key={int.id} onClick={() => toggleInt(int.id)} >
+                    <img src={int.icon} alt="" />
+                    <h3> {int.name} </h3>
+                  </div>
+                })
+              }
+            </div>
+            <button className={styles.addint} onClick={addnew}>Add</button>{/*-------------Add Your interest page open---------------*/}
+            <hr className={styles.hend} />
+            <Link to="/"><p className={styles.more}>Suggest more interest categories.</p></Link>
+
+          </>
+        } />
+
+        : <></>
       }
       {/* ------------------------------------------------------------- */}
       {/* -----------------------------------Add your interest div------------------------------------------------ */}
       {
         addnewtextdiv == true ?
-          <div className={styles.addtext}>
-            <div className={styles.int}>
-              <img src={arrow} onClick={addcrossbox} alt="" className={styles.closeinterest} />{/*-------------Close Add Your interest page ---------------*/}
-              <p className={styles.intp}>Interest</p>
-            </div>
-            <hr className={styles.head1} />
-            <div className={styles.para}>
-              <p>Didn't find your top interests?No worries! Let us know and we will
-                try our best to add more relavent
-                categories:
-              </p>
-              <input type="text" name='addtext'
-              value={interestInput}
-               onChange={(e) => setInterestInput(e.target.value)} className={styles.parainput} placeholder='Type here..'
-               />
-            </div>
-            <button className={styles.send}
-              onClick={handleAddInterest}>Send</button>
-          </div> : <></>
+          <Modal classname='max-w-[434px] rounded-[20px]'
+            body={
+              <>
+                <div className={styles.int}>
+                  <img src={arrow} onClick={addcrossbox} alt="" className={styles.closeinterest} />{/*-------------Close Add Your interest page ---------------*/}
+                  <p className={styles.intp}>Interest</p>
+                </div>
+                <hr className={styles.head1} />
+                <div className={styles.para}>
+                  <p>Didn't find your top interests?No worries! Let us know and we will
+                    try our best to add more relavent
+                    categories:
+                  </p>
+                  <input type="text" name='addtext'
+                    value={interestInput}
+                    onChange={(e) => setInterestInput(e.target.value)} className={styles.parainput} placeholder='Type here..'
+                  />
+                </div>
+                <button className={styles.send}
+                  onClick={handleAddInterest}>Send</button>
+              </>
+            } />
+          : <></>
       }
       {/* ------------------------------------------------------------------------------------------------------- */}
-    </div>
+
+    </>
   )
 }
 
