7da3c9dc16378f94a34b117b3a98ad225c16a15f
rafiqcoder <rafiqcoder@gmail.com>
2023-02-23 12:49:48 +0600

    fixed programs ui

--- src/pages/Home/Learn.js
+++ src/pages/Home/Learn.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React,{ useEffect,useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import CheckedIcon from '../../assets/icons/checked-category.svg'
 import AcivityContent from '../../components/ActivityContent/ActivityContent'
 import Filterbar from '../../components/Filterbar/filterbar'
 import ProgramCard from '../../components/ProgramCard/ProgramCard'
 import Toggle from '../../components/Toggle/Toggle'
-import Logo from '../../Images/Canva.png'
-import { getCategories, getInterests } from '../../services/activities'
-import { getAllUserPrograms, getPrograms } from '../../services/program'
-import { getMyActivitiesProgress } from '../../services/user'
-import style from './Learn.module.css'
+import { getCategories,getInterests } from '../../services/activities'
+import { getAllUserPrograms,getPrograms } from '../../services/program'
 
 const card = [
    {
@@ -46,28 +43,28 @@ const card = [
 ]
 
 const Learn = () => {
-   const [activities, setActivities] = useState([])
+   const [activities,setActivities] = useState([])
    // const [filteredActivities,setFilteredActivities] = useState([])
-   const [filterItems, setFilterItems] = useState([])
-   const [completedTabActive, setCompletedTabActive] = useState(false)
+   const [filterItems,setFilterItems] = useState([])
+   const [completedTabActive,setCompletedTabActive] = useState(false)
    // design changes if my programs is active
-   const [myPrograms, setMyPrograms] = useState(false)
+   const [myPrograms,setMyPrograms] = useState(false)
 
-   const [allPrograms, setAllPrograms] = useState([])
-   const [userPrograms, setUserPrograms] = useState([])
+   const [allPrograms,setAllPrograms] = useState([])
+   const [userPrograms,setUserPrograms] = useState([])
 
 
    const navigate = useNavigate();
    const { loggedIn } = useSelector(state => state.user)
 
-   const [onlyFreeActive, setOnlyFreeActive] = useState(true)
-   const [onlyLiveActive, setOnlyLiveActive] = useState(false)
+   const [onlyFreeActive,setOnlyFreeActive] = useState(true)
+   const [onlyLiveActive,setOnlyLiveActive] = useState(false)
 
    useEffect(() => {
       getInterests(true)
          .then(res => {
             // console.log(res.data.data);
-            setActivities(res.data.data.map(item => ({ ...item, categories: [] })))
+            setActivities(res.data.data.map(item => ({ ...item,categories: [] })))
             let temp = [
                {
                   id: 0,
@@ -75,7 +72,7 @@ const Learn = () => {
                   selected: true
                }
             ]
-            res.data.data.map((activity, idx) => {
+            res.data.data.map((activity,idx) => {
                temp.push({
                   id: activity.id,
                   children: <div className='flex items-center gap-x-1'>
@@ -90,7 +87,7 @@ const Learn = () => {
             console.log(err.response);
          })
 
-   }, [])
+   },[])
 
    useEffect(() => {
       if (activities.length === 0) return
@@ -108,7 +105,7 @@ const Learn = () => {
          }).catch(err => {
             console.log(err.response);
          })
-   }, [activities.length])
+   },[activities.length])
 
    useEffect(() => {
       getPrograms()
@@ -119,18 +116,18 @@ const Learn = () => {
          }).catch(err => {
             console.log(err.response);
          })
-   }, [])
+   },[])
 
    useEffect(() => {
       getAllUserPrograms()
          .then(res => {
-            console.log('user programs', res.data.data);
+            console.log('user programs',res.data.data);
             if (res.data.data === null) return setUserPrograms([])
             setUserPrograms(res.data.data)
          }).catch(err => {
             console.log(err.response);
          })
-   }, [])
+   },[])
 
    const onChange = (item) => {
       // console.log('item', item);
@@ -142,16 +139,16 @@ const Learn = () => {
             } else {
                sel = false
             }
-            return { ...filterItem, selected: sel }
+            return { ...filterItem,selected: sel }
          })
          setFilterItems(temp)
       } else {
 
          let temp = filterItems.map(filterItem => {
             if (filterItem.id === item.id) {
-               return { ...filterItem, selected: true }
+               return { ...filterItem,selected: true }
             } else {
-               return { ...filterItem, selected: false }
+               return { ...filterItem,selected: false }
             }
          })
          setFilterItems(temp)
@@ -165,10 +162,10 @@ const Learn = () => {
       let filteredArr = activities.filter(activity => activeIds.includes(activity.id))
       // setFilteredActivities(filteredArr)
 
-   }, [filterItems, activities])
+   },[filterItems,activities])
 
-   const [toggleButton, settoggleButton] = useState(true);
-   const [toggleButton1, settoggleButton1] = useState(true);
+   const [toggleButton,settoggleButton] = useState(true);
+   const [toggleButton1,settoggleButton1] = useState(true);
    const handleClick = () => {
       settoggleButton(!toggleButton);
       const toggle = document.querySelector(".Learn_toggle__yXQQe");
@@ -188,15 +185,16 @@ const Learn = () => {
 
 
    return (
-      <div className='lg:mx-20 lg: mt-[70px]'>
-         <div className="bg-sky-50 p-5">
-            <h1 className='text-xl font-semibold lg:hidden block'>Welcome Sahil ji! <span className='text-sm font-semibold ml-1 mt-3'> what would you like to learn today?</span></h1>
+      <div className='lg:mx-28 lg: lg:mt-[70px]'>
+         <div className="bg-sky-50 lg:bg-white p-5">
+            <h1 className='text-xl font-semibold lg:hidden block'>Hello Sahil ji! </h1>
+            <span className='text-sm ml-1 mt-3 text-gray-400'> what would you like to learn today?</span>
             <div className=" w-full flex justify-around lg:bg-white my-5" >
                <button className={`font-bold text-lg rounded-full border px-4 py-2  ${myPrograms === false && ' bg-cyan-200'}`} onClick={() => setMyPrograms(false)} >Programs</button>
                <button className={`font-bold text-lg rounded-full border px-4  ${myPrograms && ' bg-cyan-200'}`} onClick={() => setMyPrograms(true)}>My Program</button>
             </div>
          </div>
-         <h1 className='text-2xl font-semibold hidden lg:block'>Welcome Sahil ji! <span className='text-lg font-semibold ml-1 mt-3'> what would you like to learn today?</span></h1>
+         <h1 className='text-2xl font-semibold hidden lg:block ml-4'>Welcome Sahil ji! <span className='text-lg font-semibold ml-1 mt-3'> what would you like to learn today?</span></h1>
          {/* <div className='h-32  mt-4 mx-4 bg-LightSky rounded-3xl'></div> */}
          <div className="px-5">
             <AcivityContent>
@@ -224,20 +222,20 @@ const Learn = () => {
             </div>
          }
 
-         <div className='flex h-10 mx-4 mt-4'>
-            <span className='mx-4 text-xl'>only free</span>
+         <div className='flex h-10 sm:mx-4 mt-4'>
+            <span className='mx-4 lg:text-xl'>only free</span>
 
             <Toggle active={onlyFreeActive} handleClick={() => setOnlyFreeActive(!onlyFreeActive)} />
-            <span className='mx-4 text-xl'>only live</span>
+            <span className='mx-4 lg:text-xl'>only live</span>
             <Toggle active={onlyLiveActive} handleClick={() => setOnlyLiveActive(!onlyLiveActive)} />
 
          </div>
          <div className="px-5">
             <Filterbar items={filterItems} onChange={onChange} />
          </div>
-         <div className="lg:grid lg:grid-cols-3 mt-10">
+         <div className="lg:grid lg:grid-cols-3 mt-10 overflow-x-scroll" >
             {myPrograms ?
-               userPrograms.map((item, index) => (
+               userPrograms.map((item,index) => (
                   <ProgramCard key={item.id} {...item.program}
                      isUserProgram={true}
                      userProgramId={item.id}
@@ -245,7 +243,7 @@ const Learn = () => {
                      percentage_completed={item.percentage_completed}
                   />
                )) :
-               allPrograms.map((item, index) => (
+               allPrograms.map((item,index) => (
                   <ProgramCard key={item.id} {...item} />
                ))
             }
