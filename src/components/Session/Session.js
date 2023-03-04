97404ab7ede8cb612d6b2704c73cc409ac9f2980
rafiqcoder <rafiqcoder@gmail.com>
2023-03-04 19:40:02 +0600

    livepng

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import livepng from '../../assets/icons/livepng.svg'
+import liveSessionCard from '../../assets/liveSessionCard.png'
 import Photo from '../../Images/profile-pic.jfif'
 import { getFormattedDateWeek } from '../../utils/utils'
 import styles from './session.module.css'
-import liveSessionCard from '../../assets/liveSessionCard.png'
 
 export default function Session({ id,name,image,scheduled_on,host,scrollToTop }) {
 
@@ -17,6 +18,7 @@ export default function Session({ id,name,image,scheduled_on,host,scrollToTop })
    }
 
    const handleClick = () => {
+      
       navigate(`/live-events/${id}`)
       if (scrollToTop) {
          handleScrollToTop()
@@ -27,7 +29,7 @@ export default function Session({ id,name,image,scheduled_on,host,scrollToTop })
          onClick={handleClick}>
          <div className={`${styles.imgContainer} relative`}>
             <img src={liveSessionCard} className='object-cover  w-[100%] h-[180px] lg:h-[228px]' alt='session' />
-            <span className='bg-[#DD1D43] p-1 rounded-full text-white px-3 absolute top-5 right-5 z-30 '>o Live</span>
+            <div className='bg-[#DD1D43] p-1 rounded-full text-white px-2 absolute top-5 right-5 z-30 flex items-center gap-1'><img src={livepng} alt="" className='w-[10px] h-[10px] object-cover' /> <span>Live</span> </div>
 
          </div>
          <div className='px-3 pr-2 py-2 pb-5 lg:px-4 lg:py-4'>
